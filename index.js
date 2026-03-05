0a4853f746b01604904f16964a121fd983bdca56
unknownncat <semnomeyuumia@gmail.com>
2026-03-05 14:29:55 -0300

    protobuf export

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export * as crypto from './src/crypto.js';
 export * as curve from './src/curve.js';
 export * as keyhelper from './src/keyhelper.js';
+export { PreKeyWhisperMessage, WhisperMessage } from "./src/protobufs.js";
 
 export { default as ProtocolAddress } from './src/protocol_address.js';
 export { default as SessionBuilder } from './src/session_builder.js';
